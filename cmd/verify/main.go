571d743b62e0d63c0983cb7ff64d69c2272f8f4f
mickaelbsg <mickaelbsg@gmail.com>
2026-06-13 21:20:54 -0300

    Update project with latest .deb and verify utility

---
+++ cmd/verify/main.go
@@ -0,0 +1,11 @@
+package main
+import (
+    "fmt"
+    "minion/internal/security"
+)
+func main(){
+    key := "minion_sk_QrVo-_gHv9rkjyqDbgEImpjo1Work4piYcx-mV_oFvA"
+    hash := "lcwZT7fwJ_zSzlnM6S-NbBozQx4QHbdXa8pUQ7yC2tY"
+    fmt.Println("Verify result:", security.VerifyAPIKey(key, hash))
+    fmt.Println("Calculated hash:", security.HashAPIKey(key))
+}
